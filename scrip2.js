4fd2633e957098bf9ca1f17404315fbc34b08b4f
Samusdes <desdani@gmail.com>
2023-06-06 23:15:48 -0300

    Initial commit

---
+++ scrip2.js
@@ -0,0 +1,10 @@
+/* document.querySelector("button.button-menu-toggle").
+addEventListener("click", function() {
+document.querySelector(".nav-links").
+classList.toggle("nav-links-responsive")}) */
+
+document.querySelector("button.button-menu-toggle").
+addEventListener("click", function() {
+document.querySelector(".nav-links").
+classList.toggle("nav-links-responsive")}) 
+
